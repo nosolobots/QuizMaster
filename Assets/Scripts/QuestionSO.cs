8d63fc8b7d32f725179fb25135be2cee6e7ebb2b
fran-sc <fran.montoiro@gmail.com>
2025-03-25 14:00:13 +0100

    update question so

--- Assets/Scripts/QuestionSO.cs
+++ Assets/Scripts/QuestionSO.cs
@@ -3,7 +3,21 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Question", menuName = "Scriptable Objects/Quiz Question")]
 public class QuestionSO : ScriptableObject
 {
+    // Pregunta
     [TextArea(2, 5)]
     [SerializeField] 
     string question = "Introduce el texto de la pregunta aquí";
+    public string Question => question;
+
+    // Respuestas
+    [SerializeField]
+    string[] answers = new string[4];
+    public string[] Answers => answers;
+    public string GetAnswer(int index) => answers[index];
+
+    // Índice de la respuesta correcta
+    [SerializeField]
+    [Range(0, 3)]
+    int correctAnswerIndex = 0;
+    public int CorrectAnswerIndex => correctAnswerIndex;
 }
